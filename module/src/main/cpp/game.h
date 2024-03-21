323a988627e1777a0c3501d5ff2f74ae5d971e18
ale <ale@paper.com>
2024-03-21 14:34:03 +0800

    GamePackageName

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.tencent.tmgp.lv"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
